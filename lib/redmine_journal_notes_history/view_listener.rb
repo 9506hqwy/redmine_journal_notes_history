39ae7a87b4e4a42e3123ebb7e86a1a82c3a7e344
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-08-11 14:15:03 +0900

    タイミングに依存した処理を削除した。

--- lib/redmine_journal_notes_history/view_listener.rb
+++ lib/redmine_journal_notes_history/view_listener.rb
@@ -3,5 +3,6 @@
 module RedmineJournalNotesHistory
   class ViewListener < Redmine::Hook::ViewListener
     render_on :view_issues_history_journal_bottom, partial: 'journal_notes_histories/journal_bottom'
+    render_on :view_journals_update_js_bottom, partial: 'journal_notes_histories/update_journal_js_bottom'
   end
 end
